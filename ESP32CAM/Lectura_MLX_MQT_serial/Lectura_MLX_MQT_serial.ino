07d40fff6663df6c2526cd38ad86e692016b8605
“hugoescalpelo” <hugoescalpelo@gmail.com>
2022-02-14 13:42:59 -0600

    Programas base cargados

---
+++ ESP32CAM/Lectura_MLX_MQT_serial/Lectura_MLX_MQT_serial.ino
@@ -0,0 +1,21 @@
+/*
+ * Autor: Hugo Escalpelo
+ * Fecha: 14 de febrero de 2022
+ * 
+ * Este programa realiza la lecura del sensor MQ-6 y MLX90614
+ * y envia dichas lecturas por serial desde el ESP32CAM. Este
+ * se conectara por serial con la raspberry Pi, la cual enviará 
+ * los datos por MQTT al servidor (maquina virtual), en donde
+ * seran graficadas y en donde se encuentra la base de datos,
+ * etc.
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ ESP32CAM/Lectura_MLX_serial/Lectura_MLX_serial.ino
@@ -0,0 +1,37 @@
+/***************************************************
+  Autor: Hugo Escalpelo
+  Fecha: 14 de febrero de 2022
+
+  Este programa hace una comunicacinó simple
+ ****************************************************/
+//Bliblioteca
+#include <Adafruit_MLX90614.h>
+
+//Objejos
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+//Inicialización del programa
+void setup() {
+  //Iniciar comunicacion serial
+  Serial.begin(115200);
+  while (!Serial);
+
+  //Mensaje de prueba
+  Serial.println("Adafruit MLX90614 test");
+
+  //Iniciar comunicación con el sensor
+  Wire.begin (14,15);
+  if (!mlx.begin(0x5A, &Wire)) {
+    Serial.println("Error connecting to MLX sensor. Check wiring.");
+    while (1);
+  };
+}
+
+//Cuerpo del programa
+void loop() {
+  //Leer el sensor
+  Serial.println(mlx.readObjectTempC());
+
+  //Cada 500ms
+  delay(500);
+}
